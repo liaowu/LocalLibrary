52bacf4c50f15298ac8e311b319556b8fccf5838
liaowu <liaowu70@gmail.com>
2019-11-22 15:39:37 -0500

    tested

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 from django.db import models
 import uuid # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -74,15 +76,17 @@ class BookInstance(models.Model):
          )
 
         status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+        borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-class Meta:
-       ordering = ["due_back"]
-
-       def __str__(self):
-        """
-        String for representing the Model object
-        """
-        return '%s (%s)' % (self.id,self.book.title)
+        class Meta:
+            ordering = ["due_back"]
+            permissions = (("can_mark_returned", "Set book as returned"),)  
+           
+        def __str__(self):
+            """
+            String for representing the Model object
+            """
+            return '%s (%s)' % (self.id,self.book.title)
 
 class Author(models.Model):
       """
@@ -91,7 +95,7 @@ class Author(models.Model):
       first_name = models.CharField(max_length=100)
       last_name = models.CharField(max_length=100)
       date_of_birth = models.DateField(null=True, blank=True)
-      date_of_death = models.DateField('Died', null=True, blank=True)
+      date_of_death = models.DateField('death', null=True, blank=True)
           
       def get_absolute_url(self):
        """
@@ -108,3 +112,9 @@ class Author(models.Model):
 
       class Meta:
           ordering=['last_name']
+
+@property
+def is_overdue(self):
+    if self.due_back and date.today() > self.due_back:
+        return True
+    return False
